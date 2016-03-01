d27ca1e5c6a49fa50819b79531c65a2221b75249
apatil88 <amrut.patil88@hotmail.com>
2016-02-29 21:59:39 -0500

    Refactoring code

--- MakeANote/app/src/main/java/com/amrutpatil/makeanote/TrashContract.java
+++ MakeANote/app/src/main/java/com/amrutpatil/makeanote/TrashContract.java
@@ -10,24 +10,24 @@ import android.provider.BaseColumns;
 public class TrashContract {
     interface TrashColumns {
         String TRASH_ID = "_ID";
-        String TRASH_TITLE = "deleted_title";
-        String TRASH_DESCRIPTION = "deleted_description";
-        String TRASH_DATE_TIME = "deleted_date_time";
+        String TRASH_TITLE = "trash_title";
+        String TRASH_DESCRIPTION = "trash_description";
+        String TRASH_DATE_TIME = "trash_date_time";
     }
 
     public static final String CONTENT_AUTHORITY = "com.amrutpatil.makeanote.provider";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    private static final String PATH_DELETED = "deleted";
-    public static final Uri URI_TABLE = BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_DELETED).build();
+    private static final String PATH_TRASH = "trash";
+    public static final Uri URI_TABLE = BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_TRASH).build();
 
     public static class Trash implements TrashColumns, BaseColumns {
 
-        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + ".deleted";
-        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd." + CONTENT_AUTHORITY + ".deleted";
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + ".trash";
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd." + CONTENT_AUTHORITY + ".trash";
 
         //Method which enables content provider to return an individual trashed note
-        public static Uri buildDeletedUri(String deletedId) {
-            return URI_TABLE.buildUpon().appendEncodedPath(deletedId).build();
+        public static Uri buildDeletedUri(String trashId) {
+            return URI_TABLE.buildUpon().appendEncodedPath(trashId).build();
         }
 
         //Method to extract the trashed note id
