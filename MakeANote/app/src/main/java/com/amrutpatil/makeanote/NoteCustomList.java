c457dd97eebe1bbc6bd31be497942ca726c0b4cc
apatil88 <amrut.patil88@hotmail.com>
2016-03-08 20:16:20 -0500

    Method to add new checkbox

--- MakeANote/app/src/main/java/com/amrutpatil/makeanote/NoteCustomList.java
+++ MakeANote/app/src/main/java/com/amrutpatil/makeanote/NoteCustomList.java
@@ -147,6 +147,104 @@ public class NoteCustomList extends LinearLayout {
         }
     }
 
-    
+    public void setUpForListNotification(String listEntries) {
+
+        setOrientation(VERTICAL);
+        mTextBoxes = new ArrayList<>();
+        String[] listEntryTokens = listEntries.split("%");
+        boolean isStrikeOut = false;
+        String listItem = "";
+
+        for (String entryDetails : listEntryTokens) {
+            mListItem = new LinearLayout(mContext);
+            mListItem.setOrientation(HORIZONTAL);
+            //
+            if(entryDetails.contains(AppConstant.TRUE)) {
+                listItem = entryDetails.split(AppConstant.TRUE)[0];
+                isStrikeOut = true;
+            } else if(entryDetails.contains(AppConstant.FALSE)){
+                listItem = entryDetails.split(AppConstant.FALSE)[0];
+                isStrikeOut = false;
+            }
+
+            final CheckBox checkBox = new CheckBox(mContext);
+            checkBox.setChecked(isStrikeOut);
+            checkBox.setEnabled(false);
+            final EditText textBox = new EditText(mContext);
+            textBox.setText(listItem);
+            textBox.setEnabled(false);
+            textBox.setBackgroundColor(Color.TRANSPARENT);
+
+            if (isStrikeOut) {
+                textBox.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            }
+
+            mListItem.addView(checkBox);
+            mListItem.addView(textBox);
+            mTextBoxes.add(textBox);
+            addView(mListItem);
+        }
+    }
+
+    public void addNewCheckbox() {
+        mListItem = new LinearLayout(mContext);
+        mListItem.setOrientation(HORIZONTAL);
+        final ImageView deleteImageView = new ImageView(mContext);
+        deleteImageView.setImageResource(android.R.drawable.ic_menu_delete);
+
+        final EditText textBox = new EditText(mContext);
+        textBox.setBackgroundColor(Color.TRANSPARENT);
+        textBox.setHint("....");
+        textBox.setSelection(0);
+        textBox.requestFocus();
+        final CheckBox checkBox = new CheckBox(mContext);
+        checkBox.setPadding(10, 10, 10, 10);
+
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    textBox.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                } else {
+                    textBox.setPaintFlags(checkBox.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                }
+            }
+        });
+        mListItem.addView(deleteImageView);
+        mListItem.addView(checkBox);
+        mListItem.addView(textBox);
+        mTextBoxes.add(textBox);
+        addView(mListItem);
+
+        deleteImageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout layout = (LinearLayout) v.getParent();
+                mTextBoxes.remove(mTextBoxes.indexOf(layout.getChildAt(2)));
+                layout.removeAllViews();
+            }
+        });
+    }
+
+    public String getLists() {
+
+        String stringToReturn = "";
+        boolean isStrikeThrough;
+        int itemsCount = mTextBoxes.size();
+
+        for (int i = 0; i < itemsCount; i++) {
+            EditText editText = mTextBoxes.get(i);
+            if (editText.getPaintFlags() == Paint.STRIKE_THRU_TEXT_FLAG)
+                isStrikeThrough = true;
+            else
+                isStrikeThrough = false;
+            if (editText.getText().toString().length() > 0)
+                stringToReturn = stringToReturn + editText.getText().toString() + "$" + isStrikeThrough + "%";
+        }
+
+        return stringToReturn;
+    }
+
 
 }
