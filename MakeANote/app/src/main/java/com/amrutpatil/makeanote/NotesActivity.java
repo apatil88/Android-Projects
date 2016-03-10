247f13952aa81ba924647fa31c7396f394327361
apatil88 <amrut.patil88@hotmail.com>
2016-03-09 19:40:42 -0500

    Added method to retrieve image from Dropbox

--- MakeANote/app/src/main/java/com/amrutpatil/makeanote/NotesActivity.java
+++ MakeANote/app/src/main/java/com/amrutpatil/makeanote/NotesActivity.java
@@ -3,6 +3,8 @@ package com.amrutpatil.makeanote;
 import android.content.ContentResolver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -12,11 +14,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.exception.DropboxException;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -154,8 +161,112 @@ public class NotesActivity extends BaseActivity implements LoaderManager.LoaderC
                         }while(mIsImageNotFound);
                     }
                 });
+            } else if (AppConstant.DROP_BOX_SELECTION == aNote.getStorageSelection()){
+                threads[threadCounter] = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        do{
+                            Drawable drawable = getImageFromDropbox(mDropboxAPI,
+                                    AppSharedPreferences.getDropBoxUploadPath(getApplicationContext()),
+                                    aNote.getImagePath());
+                            if(drawable != null) {
+                                Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+                                aNote.setBitmap(bitmap);
+                            }
+                            if(!mIsImageNotFound){
+                                mNotesAdapter.setData(mNotes);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mNotesAdapter.notifyImageObtained();
+                                    }
+                                });
+                            }
+                            try{
+                                Thread.sleep(500);
+                            } catch (InterruptedException e){
+                                e.printStackTrace();
+                            }
+
+                        }while(mIsImageNotFound);
+                    }
+                });
+                threads[threadCounter].start();
+                threadCounter++;
+            } else {
+                aNote.setHasNoImage(true);
             }
         }
-
+        mNotesAdapter.setData(mNotes);  //Adapter has the latest copy of note
+        changeNoItemTag();
     }
+
+    //Method checks if the path to file is valid
+    //Method looks for the file we are looking for, grabs it and returns it to the calling process
+    private Drawable getImageFromDropbox(DropboxAPI<?> mApi, String mPath, String filename) {
+        FileOutputStream fos;
+        Drawable drawable;
+        //Check to see if we have cached the file on the local device. If we found the file on the device, retrieve it
+        String cachePath = getApplicationContext().getCacheDir().getAbsolutePath() + "/" + filename;
+        File cacheFile = new File(cachePath);
+        if (cacheFile.exists()) {
+            mIsImageNotFound = false;
+            return Drawable.createFromPath(cachePath);
+        } else { //If we did not find the file, go to Dropbox to retrieve it
+            try {
+                DropboxAPI.Entry dirEnt = mApi.metadata(mPath, 1000, null, true, null);
+                //If the path is not a directory path or if it is null
+                if (!dirEnt.isDir || dirEnt.contents == null) {
+                    mIsImageNotFound = true;
+                }
+                ArrayList<DropboxAPI.Entry> thumbs = new ArrayList<DropboxAPI.Entry>();
+                for (DropboxAPI.Entry ent : dirEnt.contents) {
+                    if (ent.thumbExists) {
+                        if (ent.fileName().startsWith(filename)) {
+                            thumbs.add(ent);
+                        }
+                    }
+                }
+                if (thumbs.size() == 0) {
+                    mIsImageNotFound = true;
+                } else {
+                    //Grab the image from Dropbox
+                    DropboxAPI.Entry ent = thumbs.get(0);
+                    String path = ent.path;
+                    try {
+                        fos = new FileOutputStream(cachePath);
+
+                    } catch (FileNotFoundException e) {
+                        return getResources().getDrawable(R.drawable.ic_image_deleted);
+                    }
+                    mApi.getThumbnail(path, fos, DropboxAPI.ThumbSize.BESTFIT_960x640,
+                            DropboxAPI.ThumbFormat.JPEG, null);
+                    drawable = Drawable.createFromPath(cachePath);
+                    mIsImageNotFound = false;
+                    return drawable;
+                }
+            } catch (DropboxException e) {
+                e.printStackTrace();
+                mIsImageNotFound = true;
+            }
+
+            drawable = getResources().getDrawable(R.drawable.ic_loading);
+            return drawable;
+        }
+    }
+
+    //Method to show/hide the RecyclerView depending on item availability
+    private void changeNoItemTag(){
+        TextView mItemTextView = (TextView) findViewById(R.id.no_item_textview);
+        //if there are items to show
+        if(mNotesAdapter.getItemCount() != 0){
+            mItemTextView.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
+        }else{
+            mItemTextView.setText(AppConstant.EMPTY);
+            mItemTextView.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
+        }
+    }
+
 }
