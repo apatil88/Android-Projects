3de81f9ee4bebba2da147f312944e413a84a4b9a
apatil88 <amrut.patil88@hotmail.com>
2016-03-13 19:36:05 -0400

    Refactoring

--- MakeANote/app/src/main/java/com/amrutpatil/makeanote/AlarmReceiver.java
+++ MakeANote/app/src/main/java/com/amrutpatil/makeanote/AlarmReceiver.java
@@ -1,14 +1,14 @@
 package com.amrutpatil.makeanote;
 
 
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 /**
  * Description: Class to generate Alarm notification.
@@ -45,8 +45,8 @@ public class AlarmReceiver extends BroadcastReceiver {
                 .setContentText(description);
 
         builder.setContentIntent(resultPendingIntent);
-        NotificationManagerCompat notificationManagerCompat =
-                (NotificationManagerCompat) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManagerCompat.notify(aNote.getId(), builder.build());
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(aNote.getId(), builder.build());
     }
 }
